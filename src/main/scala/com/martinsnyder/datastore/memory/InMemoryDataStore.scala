5d3df578bd828be13a400aaf9e4fe57ad8867474
Martin.Snyder <msnyder@wingspan.com>
2014-05-14 13:42:05 -0400

    Added Transaction Support

--- src/main/scala/com/martinsnyder/datastore/memory/InMemoryDataStore.scala
+++ src/main/scala/com/martinsnyder/datastore/memory/InMemoryDataStore.scala
@@ -5,41 +5,9 @@ import scala.util.Try
 import scala.reflect.ClassTag
 import com.martinsnyder.datastore.DataStore.ConstraintViolation
 
-class InMemoryDataStore(constraints: Seq[Constraint] = Nil) extends DataStore {
-  var storedRecords = List[Record]()
-  var constraintEnforcers: List[ConstraintEnforcer] = constraints.map(ConstraintEnforcer(_)).toList
-
-  override def withConnection[T](f: (ReadConnection) => T): T = f(new InMemoryReadConnection)
-
-  private def filter(recordClass: Class[_], condition: Condition) = synchronized(Try(
-    storedRecords
-      .filter(record => record.getClass == recordClass)
-      .filter(record => condition match {
-        case EqualsCondition(fieldName, value) =>
-          record.getClass.getMethod(fieldName).invoke(record) == value
-      }))
-  )
-
-  private def applyConstraints(records: Seq[Record]): Try[List[ConstraintEnforcer]] = {
-    val triedEnforcers = constraintEnforcers.map(_.add(records))
-    triedEnforcers.find(_.isFailure) match {
-      case Some(failure) =>
-        failure.map(List(_))
-      case None =>
-        Try(triedEnforcers.map(_.get))
-    }
-  }
-
-  private def addRecords(records: Seq[Record]): Try[Unit] = synchronized(
-    for (
-      nextEnforcers <- applyConstraints(records);
-      nextRecords <- Try(records.toList ::: storedRecords)
-    )
-    yield {
-      storedRecords = nextRecords
-      constraintEnforcers = nextEnforcers
-    }
-  )
+object InMemoryDataStore {
+  sealed trait TransactionOperation
+  case class InsertOperation(records: Seq[Record]) extends TransactionOperation
 
   sealed trait ConstraintEnforcer {
     def add(records: Seq[Record]): Try[ConstraintEnforcer]
@@ -66,16 +34,93 @@ class InMemoryDataStore(constraints: Seq[Constraint] = Nil) extends DataStore {
     })
   }
 
+  class ConstrainedRecords(private var storedRecords: List[Record],
+                           private var constraintEnforcers: List[ConstraintEnforcer]) {
+    def filter(recordClass: Class[_], condition: Condition) = synchronized(Try(
+      storedRecords
+        .filter(record => record.getClass == recordClass)
+        .filter(record => condition match {
+        case EqualsCondition(fieldName, value) =>
+          record.getClass.getMethod(fieldName).invoke(record) == value
+      }))
+    )
+
+    private def applyConstraints(records: Seq[Record]): Try[List[ConstraintEnforcer]] = {
+      val triedEnforcers = constraintEnforcers.map(_.add(records))
+      triedEnforcers.find(_.isFailure) match {
+        case Some(failure) =>
+          failure.map(List(_))
+        case None =>
+          Try(triedEnforcers.map(_.get))
+      }
+    }
+
+    def addRecords(records: Seq[Record]): Try[Unit] = synchronized(
+      for (
+        nextEnforcers <- applyConstraints(records);
+        nextRecords <- Try(records.toList ::: storedRecords)
+      )
+      yield {
+        storedRecords = nextRecords
+        constraintEnforcers = nextEnforcers
+      }
+    )
+
+    def applyOperations(operations: Seq[TransactionOperation]): Try[Unit] = synchronized({
+      val triedOperations = operations.map({
+        case InsertOperation(records) => addRecords(records)
+      })
+
+      triedOperations.find(_.isFailure) match {
+        case Some(failure) =>
+          failure.map(List(_))
+        case None =>
+          Try(())
+      }
+    })
+
+    def copy = synchronized(new ConstrainedRecords(storedRecords, constraintEnforcers))
+  }
+}
+
+class InMemoryDataStore(constraints: Seq[Constraint] = Nil) extends DataStore {
+  import InMemoryDataStore._
+
+  val constrainedRecords = new ConstrainedRecords(Nil, constraints.map(ConstraintEnforcer(_)).toList)
+
+  override def withConnection[T](f: (ReadConnection) => T): T = f(new InMemoryReadConnection)
+
   class InMemoryReadConnection extends ReadConnection {
     override def loadRecords[T <: Record](condition: Condition)(implicit recordTag: ClassTag[T]) =
-      filter(recordTag.runtimeClass, condition)
+      constrainedRecords.filter(recordTag.runtimeClass, condition)
 
-    override def inTransaction[T](f: (WriteConnection) => Try[T]): Try[T] =
-      f(new InMemoryWriteConnection)
+    override def inTransaction[T](f: (WriteConnection) => Try[T]): Try[T] = {
+      val writeConnection = new InMemoryWriteConnection(constrainedRecords)
+
+      for (
+        invocationResult <- f(writeConnection);
+        _ <- writeConnection.commit
+      ) yield invocationResult
+    }
   }
 
-  class InMemoryWriteConnection extends InMemoryReadConnection with WriteConnection {
-    override def insertRecords[T <: Record](records: Seq[Record]): Try[Unit] =
-      addRecords(records)
+  class InMemoryWriteConnection(targetContext: ConstrainedRecords) extends WriteConnection {
+    val transactionContext = targetContext.copy
+    var transactionOperations = List[TransactionOperation]()
+
+    override def loadRecords[T <: Record](condition: Condition)(implicit recordTag: ClassTag[T]) = synchronized(
+      transactionContext.filter(recordTag.runtimeClass, condition))
+
+    override def insertRecords[T <: Record](records: Seq[Record]): Try[Unit] = synchronized({
+      transactionContext.addRecords(records).map(_ => {
+        transactionOperations = InsertOperation(records) :: transactionOperations
+        ()
+      })
+    })
+
+    override def inTransaction[T](f: (WriteConnection) => Try[T]): Try[T] =
+      f(new InMemoryWriteConnection(synchronized(transactionContext)))
+
+    def commit: Try[Unit] = targetContext.applyOperations(transactionOperations.reverse)
   }
 }
--- src/test/scala/com/martinsnyder/datastore/test/AbstractDataStoreTest.scala
+++ src/test/scala/com/martinsnyder/datastore/test/AbstractDataStoreTest.scala
@@ -2,7 +2,7 @@ package com.martinsnyder.datastore.test
 
 import org.scalatest.FunSpec
 import com.martinsnyder.datastore.{EqualsCondition, DataStore, Record}
-import scala.util.{Failure, Success}
+import scala.util.{Try, Failure, Success}
 import com.martinsnyder.datastore.DataStore.ConstraintViolation
 
 object AbstractDataStoreTest {
@@ -15,14 +15,14 @@ abstract class AbstractDataStoreTest extends FunSpec {
   val dataStore: DataStore
 
   describe("DataStore") {
-    it ("lets me insert a record") {
+    it("lets me insert a record") {
       val myRecord = MyRecord("testInsert")
 
       val insertResult = dataStore.withConnection(_.inTransaction(_.insertRecords(Seq(myRecord))))
       assert(insertResult.isSuccess)
     }
 
-    it ("lets me retrieve a stored record") {
+    it("lets me retrieve a stored record") {
       val myRecord = MyRecord("testRetrieve")
 
       val insertResult = dataStore.withConnection(_.inTransaction(_.insertRecords(Seq(myRecord))))
@@ -32,7 +32,7 @@ abstract class AbstractDataStoreTest extends FunSpec {
       assert(records == Success(Seq(myRecord)))
     }
 
-    it ("disallows duplicate records") {
+    it("disallows duplicate records") {
       val myRecord = MyRecord("testDuplicate")
 
       val insertResult = dataStore.withConnection(_.inTransaction(_.insertRecords(Seq(myRecord))))
@@ -42,6 +42,46 @@ abstract class AbstractDataStoreTest extends FunSpec {
         case Failure(ConstraintViolation(_)) => // Good
         case _ => fail("duplicate insert allowed")
       }
+    }
+
+    it("supports transaction rollback") {
+      val myRecord = MyRecord("testTransactionRollback")
+
+      val transactionResult = dataStore.withConnection(_.inTransaction(connection => Try({
+        val insertResult = connection.insertRecords(Seq(myRecord))
+        assert(insertResult.isSuccess)
+
+        val records = connection.loadRecords[MyRecord](EqualsCondition("value", "testTransactionRollback"))
+        assert(records == Success(Seq(myRecord)))
+
+        throw new Exception("whoops!")
+      })))
+
+      assert(transactionResult.isFailure)
+
+      val records = dataStore.withConnection(_.loadRecords[MyRecord](EqualsCondition("value", "testTransactionRollback")))
+      assert(records == Success(Seq()))
+    }
+
+    it("rejects conflicting transactions") {
+      val myRecord = MyRecord("testTransactionConflict")
+
+      dataStore.withConnection(connection => {
+        val transaction1Result = connection.inTransaction(writeConnection1 => Try({
+          val insertResult1 = writeConnection1.insertRecords(Seq(myRecord))
+          assert(insertResult1.isSuccess)
+
+
+          val transaction2Result = connection.inTransaction(writeConnection2 => Try({
+            val insertResult2 = writeConnection2.insertRecords(Seq(myRecord))
+            assert(insertResult2.isSuccess)
+          }))
+
+          assert(transaction2Result.isSuccess)
+        }))
+
+        assert(transaction1Result.isFailure)
+      })
     }
   }
 }
